e1adc4a79a39d36673ac6d7e4511e09d8289c7c3
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-05-02 14:01:06 +0200

    bug fixes on project

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,5 +1,6 @@
 const Photo = require('../models/photo.model');
-
+const Voter = require("../models/Voter.model");
+const requestIp = require("request-ip")
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
@@ -10,11 +11,37 @@ exports.add = async (req, res) => {
 
 		if (title && author && email && file) { // if fields are not empty...
 
+			const authorPattern = new RegExp(
+				/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+				"g"
+			);
+			const emailPattern = new RegExp(
+				/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+				"g"
+			);
+
+			const titlePattern = new RegExp(
+				/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+				"g"
+			);
+
+			if (!authorPattern.test(author)) {
+				throw new Error("Invalid author");
+			}
+			if (!emailPattern.test(email)) {
+				throw new Error("Invalid email");
+			}
+			if (!titlePattern.test(title)) {
+				throw new Error("Invalid title");
+			}
+
 			const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
 			const fileExt = fileName.split('.').slice(-1)[0];
 
-			if (fileExt[1] === '.jpg' || '.png' || '.gif') {
-
+			if ((fileExt[1] === '.jpg' || '.png' || '.gif') &&
+				title.length <= 25 &&
+				author.length <= 50
+			) {
 				const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
 				await newPhoto.save(); // ...save new photo in DB
 				res.json(newPhoto);
@@ -49,15 +76,31 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
-    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
-    else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
-    }
-  } catch(err) {
-    res.status(500).json(err);
-  }
+	  const userIp = requestIp.getClientIp(req);
+	  const findUser = await Voter.findOne({ user: userIp })
+	  const photoToUpdate = await Photo.findOne({ _id: req.params.id });
 
+	  if (findUser) {
+		  // if user already voted...
+		  if (findUser.votes.includes(photoToUpdate._id)) {
+			  res.status(500).json({ message: "Error..." });
+			} else {
+				// if user has not voted yet...
+				photoToUpdate.votes += 1;
+				await photoToUpdate.save();
+				findUser.votes.push(photoToUpdate._id);
+				await findUser.save();
+				res.json(photoToUpdate);
+			}
+		} else {
+			// if user has not voted yet...
+			const newVoter = new Voter({ user: userIp, votes: [photoToUpdate._id] });
+			await newVoter.save();
+			photoToUpdate.votes += 1;
+			await photoToUpdate.save();
+			res.json(photoToUpdate);
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
